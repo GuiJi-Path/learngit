0bee59d1b90c1135ce20c77e798bb1d3d09d456c
GuiJi_Path <GuiJi_Path@hotmail.com>
2021-01-30 17:05:37 +0800

    1

---
+++ awesome-python3-webapp/www/templates/app.py
@@ -0,0 +1,36 @@
+# import logging; logging.basicConfig(level=logging.INFO)
+# import asyncio, os, json, time
+# from datetime import datetime
+# from aiohttp import web
+# def index(request):
+#     return web.Response(body=b'<h1>Awesome</h1>')
+#
+# @asyncio.coroutine
+# def init(loop):
+#     app=web.Application(loop=loop)
+#     app.router.add_route('GET','/',index)
+#     srv=yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
+#     logging.info('server started at http://127.0.0.1:9000')
+#     return srv
+#
+# loop=asyncio.get_event_loop()
+# loop.run_until_complete(init(loop))
+# loop.run_forever()
+#-----------------------------------------------------------------
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+from datetime import datetime
+from aiohttp import web
+def index(request):
+    return web.Response(body=b'<h1>Welcome</h1>',content_type='text/html')
+async def init(loop):
+    app=web.Application()
+    app.router.add_route('GET', '/', index)
+    apprunner=web.AppRunner(app)
+    await apprunner.setup()
+    srv=await loop.create_server(apprunner.server,'127.0.0.1',9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+loop=asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
